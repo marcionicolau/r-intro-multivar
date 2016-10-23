ca8c7052d6d3b77917db385c0d705d1549a33f94
Marcio Nicolau <marcionicolau@me.com>
2016-10-23 00:08:53 -0200

    Update config files for travis-ci

--- _deploy.sh
+++ _deploy.sh
@@ -1,16 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+# configure your name and email if you have not done so
+git config --global user.email "marcionicolau@me.com"
+git config --global user.name "Marcio Nicolau"
 
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
-
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+# clone the repository to the book-output directory
+git clone -b gh-pages \
+  https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
 cd book-output
 cp -r ../_book/* ./
-git add --all *
-git commit -m"Update the book" || true
+git add *
+git commit -m"Update the book"
 git push origin gh-pages
